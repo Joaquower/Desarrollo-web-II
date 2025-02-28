4743fe5d004dcb25f9b02b69674af25c0d5b84f5
Joaquower <joaquin.casillas02@gmail.com>
2025-02-28 13:11:11 -0600

    titulo agregado en evento

--- Examen/examen/urls.py
+++ Examen/examen/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views  # Importamos las vistas correctamente
 
 urlpatterns = [
-    path("eventos/", views.eventos, name="eventos"),  # Página de eventos
-    path("boletos/", views.boletos, name="boletos"),  # Página de boletos
-    path("", views.home, name="home"),  # Página de boletos
+    path("eventos/", views.eventos, name="eventos"), 
+    path("boletos/", views.boletos, name="boletos"), 
+    path("", views.home, name="home"),  
 
 ]
--- Examen/examen/views.py
+++ Examen/examen/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render, get_object_or_404
 from .models import Evento, Boleto
 from django.shortcuts import render, get_object_or_404, redirect
 
-# 📌 Mostrar la lista de eventos
+
 def eventos(request):
-    eventos = Evento.objects.all()  # Obtiene todos los eventos
+    eventos = Evento.objects.all()  
     return render(request, "eventos.html", {"eventos": eventos})
 
-# 📌 Mostrar la lista de boletos
+
 def boletos(request):
-    boletos = Boleto.objects.all()  # Obtiene todos los boletos
+    boletos = Boleto.objects.all()  
     return render(request, "boletos.html", {"boletos": boletos})
 
-# 📌 Página de inicio
+
 def home(request):
     return render(request, "home.html")
